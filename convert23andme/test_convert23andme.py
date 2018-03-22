40a53aac3cb2c910992cac88c8f5fc9064ed6747
Tomer Altman <analytics@tomeraltman.net>
2018-03-22 12:04:12 -0700

    Init commit of unittests.

---
+++ convert23andme/test_convert23andme.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+import os, sys, unittest, convert23andme
+
+class Test_parse_23andMe_file(unittest.TestCase):
+
+    def test_parse(self):
+        self.assertTrue(convert23andme.parse_23andMe_file('shorttest_deadbeef.txt'))
+
+
+class Test_convert_23andme_bcf(unittest.TestCase):
+
+    def test_conversion(self):
+        self.assertTrue(convert23andme.convert_23andme_bcf('shorttest_deadbeef.txt',
+                                                              'data/human_g1k_v37.fasta.gz',
+                                                              'convert23andme/ucsc-gene-symbols-coords.txt.gz',
+                                                              '/tmp')
+
+
+
+if __name__ == '__main__':
+    unittest.main()
