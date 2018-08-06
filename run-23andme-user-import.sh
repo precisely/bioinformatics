6e2a62b20ab78f6d44aa57142680f0feaae06c16
Constantine Vetoshev <vetoshev@gmail.com>
2018-08-06 07:19:36 -0700

    Double-down on destination no-clobber.

--- run-23andme-user-import.sh
+++ run-23andme-user-import.sh
@@ -49,6 +49,17 @@ fi
 
 
 ### run
+function avoid_dest_overwrite {
+    local phase=$1
+    if [[ ! -z $(aws s3 --endpoint-url "${AWS_S3_ENDPOINT_URL}" ls "s3://${S3_BUCKET_GENETICS_VCF}/${PARAM_SAMPLE_ID}") ]]; then
+        echo "${PARAM_SAMPLE_ID} already exists in S3 (${phase})" 1>&2
+        exit 1
+    fi
+}
+
+# Do not do any expensive work if the destination path exists in S3.
+avoid_dest_overwrite "check before conversion and imputation"
+
 workdir="./$(date +"%Y-%m-%d.%H-%M-%S.%N")"
 mkdir "${workdir}"
 pushd "${workdir}" > /dev/null
@@ -73,12 +84,8 @@ done
 
 popd > /dev/null
 
-# Do not clobber destination! Might be nice to do this before running expensive
-# imputations, too.
-if [[ ! -z $(aws s3 --endpoint-url "${AWS_S3_ENDPOINT_URL}" ls "s3://${S3_BUCKET_GENETICS_VCF}/${PARAM_SAMPLE_ID}") ]]; then
-    echo "${PARAM_SAMPLE_ID} already exists in S3" 1>&2
-    exit 1
-fi
+# Once again, check that the destination does not exist in case another process raced this one.
+avoid_dest_overwrite "check after imputation and before final copy into S3"
 
 src_dir=$(readlinkf "${PARAM_SAMPLE_ID}")
 aws s3 --endpoint-url "${AWS_S3_ENDPOINT_URL}" cp --recursive "${src_dir}" "s3://${S3_BUCKET_GENETICS_VCF}/${PARAM_SAMPLE_ID}" --exclude "23andme-raw.txt" > /dev/null
