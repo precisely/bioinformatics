cadb8eb22a5ce4dd6fba483697c6d2d34539a051
Constantine Vetoshev <vetoshev@gmail.com>
2018-08-16 12:31:11 -0700

    Make genotype imputation include only the required chromosomes in the output.
    Previously, imputation copied unrelated chromosomes from the input into the
    output. Also, Y and MT imputation does not work, but we still need output files.
    Copy that data from the input file.

--- impute-genotype.sh
+++ impute-genotype.sh
@@ -6,7 +6,7 @@ set -o pipefail
 
 ### parameters
 if [[ "$#" -eq 0 ]]; then
-    echo "usage: impute-genotype.sh <input-vcf-path> <output-imputed-vcf-path> all|<num-chromosome>? <num-cores>? <test-mode>?" 1>&2
+    echo "usage: impute-genotype.sh <input-vcf-path> <output-imputed-vcf-path> <num-chromosome> <num-cores>? <test-mode>?" 1>&2
     exit 1
 fi
 
@@ -32,7 +32,8 @@ if [[ -z "${output_imputed_vcf_path}" ]]; then
 fi
 
 if [[ -z "${num_chromosome}" ]]; then
-    num_chromosome=all
+    echo "chromosome to impute is required (1..22, X, Y, MT)" 1>&2
+    exit 1
 fi
 
 if [[ -z "${num_cores}" ]]; then
@@ -53,14 +54,29 @@ beagle_leash=/precisely/beagle-leash/inst/beagle-leash/bin/beagle-leash
 if [[ -e "${output_imputed_vcf_path}*" ]]; then
     echo "${output_imputed_vcf_path} already exists, no imputation attempted"
 else
-    if [[ "${num_chromosome}" != "all" ]]; then
-        export BEAGLE_LEASH_CHROMS=${num_chromosome}
-    fi
     if [[ "${test_mode}" == "true" ]]; then
         printf "##input=${input_vcf_path}\n##chromosome=${num_chromosome}\ninput: ${input_vcf_path}\nchromosome: ${num_chromosome}\n" > "${output_imputed_vcf_path}.bgz"
         printf "sample tabix index\n" > "${output_imputed_vcf_path}.bgz.tbi"
     else
-        "${beagle_leash}" "${input_vcf_path}" "${output_imputed_vcf_path}.gz" ${num_cores}
+        if [[ "${num_chromosome}" == "Y" || "${num_chromosome}" == "MT" ]]; then
+            # Imputation is not supported for Y and MT chromosomes; for these,
+            # we just copy them from the input to the output file.
+            zcat "${input_vcf_path}" | \
+                awk -v chr="${num_chromosome}" '/^#/ || $1 == chr' | \
+                gzip > "${output_imputed_vcf_path}.gz"
+        else
+            export BEAGLE_LEASH_CHROMS=${num_chromosome}
+            "${beagle_leash}" "${input_vcf_path}" "${output_imputed_vcf_path}-tmp.gz" ${num_cores}
+            # remove entries from the output file which do not match the required chromosome
+            if [[ ! -f "${output_imputed_vcf_path}-tmp.gz" ]]; then
+                echo "${output_imputed_vcf_path}-tmp.gz missing!" 1>&2
+                exit 1
+            fi
+            zcat "${output_imputed_vcf_path}-tmp.gz" | \
+                awk -v chr="${num_chromosome}" '/^#/ || $1 == chr' | \
+                gzip > "${output_imputed_vcf_path}.gz"
+            rm -f "${output_imputed_vcf_path}-tmp.gz"
+        fi
         zcat "${output_imputed_vcf_path}.gz" | bgzip > "${output_imputed_vcf_path}.bgz"
         rm -f "${output_imputed_vcf_path}.gz"
         tabix -p vcf "${output_imputed_vcf_path}.bgz"
