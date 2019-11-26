1ed092e430343a29118c8a248058267eb72e92d7
Constantine Vetoshev <vetoshev@gmail.com>
2019-11-26 14:17:08 -0800

    Add Gareth's user area.

---
+++ terraform/gareth/main.tf
@@ -0,0 +1,39 @@
+provider "aws" {
+  region = "us-west-1"
+}
+
+
+terraform {
+  backend "s3" {
+    bucket = "precisely-terraform-state-biodev"
+    key = "gareth/terraform.tfstate"
+    region = "us-west-1" # save all TF state in us-west-1, not the CF region!
+    dynamodb_table = "terraform-locks-biodev"
+    encrypt = true
+  }
+}
+
+
+variable "machine_count" {
+  type = number
+  default = 1
+}
+
+
+variable "ebs_size_gb" {
+  type = number
+  default = 1005
+}
+
+
+module "cluster" {
+  source = "../modules/compute-farm"
+
+  cluster_name = "gareth-cf-cluster"
+  availability_zone = "us-west-1a"
+  instance_type = "m5.16xlarge"
+  machine_count = var.machine_count
+  ssh_public_key_path = "/Users/gareth/.ssh/precisely_aws_biodev.pub"
+  ebs_size_gb = var.ebs_size_gb
+  data_s3_bucket = "precisely-bio-data-norcal"
+}
